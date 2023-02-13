1575eb82030aa211a120b822b2fdf124bb9c8693
lify <fly72109@163.com>
2023-02-13 16:40:30 +0800

    employee

--- src/views/employee/index.vue
+++ src/views/employee/index.vue
@@ -3,7 +3,45 @@
  * @Author: Fancyicookie
 -->
 <template>
-  <div>员工管理</div>
+  <div id="member-app" class="container">
+    <div class="table-container">
+      <div class="tableBar">
+        <div class="el-input el-input--prefix el-input--suffix" style="width:250px">
+          <input type="text" placeholder="请输入员工姓名" class="el-input__inner">
+          <span class="el-input__prefix">
+            <i class="el-input__icon el-icon-search" />
+          </span>
+        </div>
+        <button class="el-button el-button--primary">
+          <span>+ 添加员工</span>
+        </button>
+      </div>
+      <div class="el-table tableBox">
+        <el-table :data="tableList" stripe border style="width: 100%">
+          <el-table-column prop="name" label="员工姓名" width="180" />
+          <el-table-column prop="username" label="账号" width="180" />
+          <el-table-column prop="phone" label="手机号" width="180" />
+          <!-- 需要单独设置 -->
+          <el-table-column prop="status" label="账号状态" width="180">
+            <template slot-scope="scope">
+              <span>{{ scope.row.status == 1 ? '已启用' : '禁用' }}</span>
+            </template>
+          </el-table-column>
+          <el-table-column prop="operation" label="操作">
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+              <el-button
+                size="mini"
+                type="danger"
+                @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -22,7 +60,9 @@ export default {
     fetchData() {
       // 这里参数不可变，之后需要改为 动态改变的写法
       getEmployees({ page: 1, pageSize: 10 }).then(res => {
-        console.log(res)
+        // return res
+        console.log(res.data.records)
+        this.tableList = res.data.records
       })
     }
   }
@@ -31,5 +71,17 @@ export default {
 </script>
 
 <style>
-
+.container {
+  padding: 20px;
+  background-color: rgb(242, 242, 242);
+}
+.container .table-container {
+  padding: 25px;
+  background-color: #fff;
+}
+.container .tableBar {
+  display: flex;
+  margin-bottom: 20px;
+  justify-content: space-between;
+}
 </style>
